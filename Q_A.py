f3f6cced4b0d28cc09ec090969343960e42beff4
WOKNz <sashafix@gmail.com>
2019-07-02 09:58:59 +0300

    Small improvements and fixes

--- Q_A.py
+++ Q_A.py
@@ -26,16 +26,38 @@ if __name__ == '__main__':
             triangles.append(Triangle(points[int(fields[1]) - 1], points[int(fields[2]) - 1],
                                       points[int(fields[3].replace('\n', '')) - 1],
                                       int(fields[0])))
-
+    plt.axes().set_aspect('equal')
     # Plotting both points and triangles (with duplicates)
     for i in range(0, len(points)):
         plt.plot(points[i].x, points[i].y, 'bo--')
     for i in range(0, len(triangles)):
         plt.plot([triangles[i].p1.x, triangles[i].p2.x, triangles[i].p3.x, triangles[i].p1.x],
                  [triangles[i].p1.y, triangles[i].p2.y,
-                  triangles[i].p3.y, triangles[i].p1.y], 'r')
+                  triangles[i].p3.y, triangles[i].p1.y], 'b')
     plt.show()
 
+    for i in range(0, len(triangles) - 1):
+        for j in range(i + 1, len(triangles) - 1):
+            if (triangles[i].list_of_points[0] in triangles[j].list_of_points) and (
+                    triangles[i].list_of_points[1] in triangles[j].list_of_points):
+                triangles[i].t1 = triangles[j]
+            if (triangles[i].list_of_points[0] in triangles[j].list_of_points) and (
+                    triangles[i].list_of_points[2] in triangles[j].list_of_points):
+                triangles[i].t2 = triangles[j]
+            if (triangles[i].list_of_points[1] in triangles[j].list_of_points) and (
+                    triangles[i].list_of_points[2] in triangles[j].list_of_points):
+                triangles[i].t3 = triangles[j]
+
+    # list_of_contains = []
+    # for i, triangle in enumerate(triangles):
+    #     list_of_contains.append(triangle.list_of_points)
+    #
+    # for triangle in triangles:
+    #     if triangle.p1.id, triangles) and a, list):
+    #
+    # Voronoi_vertices = []
+    #
+    print('test')
     # c = np.array([(0, 0, 1, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
     #
     # lc = mc.LineCollection(lines, colors=c, linewidths=2)
--- geometry.py
+++ geometry.py
@@ -7,8 +7,12 @@ class Point:  # Class of Point with option to have ID
 
 
 class Triangle:  # Class of triangle with option to have ID
-    def __init__(self, p1: Point, p2: Point, p3: Point, sid=None):
+    def __init__(self, p1: Point, p2: Point, p3: Point, sid=None, t1=None, t2=None, t3=None):
         self.p1 = p1
         self.p2 = p2
         self.p3 = p3
         self.id = sid
+        self.t1 = t1
+        self.t2 = t2
+        self.t3 = t3
+        self.list_of_points = [p1.id, p2.id, p3.id]
