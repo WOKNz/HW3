cccd6bdf4e3a843106e5040c98f794252e32c915
WOKNz <sashafix@gmail.com>
2019-07-02 23:24:50 +0300

    Final Commit

--- Q_A.py
+++ Q_A.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from geometry import Point
 from geometry import Triangle
@@ -7,8 +8,7 @@ if __name__ == '__main__':
 
     points = []
     triangles = []
-    lines = [[(0, 1), (1, 1)], [(2, 3), (3, 3)], [(1, 2), (1, 3)]]
-    const = 1000.0
+    const = 1000000.0  # Constant used to calculate infinity far points of Voronoi
 
     # Reading points
     with open('points.txt', "r", encoding="utf8") as stringlines:
@@ -36,8 +36,9 @@ if __name__ == '__main__':
                  [triangles[i].p1.y, triangles[i].p2.y,
                   triangles[i].p3.y, triangles[i].p1.y], 'b')
     # plt.show()
-    axes = plt.axis()
+    axes = plt.axis()  # Saving ratio of plot axes
 
+    # Adding closest Triangles to each Triangle
     for i in range(0, len(triangles)):
         for j in range(0, len(triangles)):
             if j == i:
@@ -53,7 +54,8 @@ if __name__ == '__main__':
                 triangles[i].t3 = triangles[j]
 
 
-    def cntoftrg(t: Triangle):  # Solving center of triangle by 3 points (two perpendicular lines)
+    # Solving center of triangle by 3 points (two perpendicular lines)
+    def cntoftrg(t: Triangle):
         def slope(p1: Point, p2: Point):
             return (p2.y - p1.y) / (p2.x - p1.x)
 
@@ -64,7 +66,9 @@ if __name__ == '__main__':
         return Point(x_value, y_value)
 
 
+    # Returning Point of orthogonal line that goes to infinity (depends on const variable)
     def infpoint(p1, p2, p3, center):
+        # Calculating slope
         def slope(p1: Point, p2: Point):
             if p1.x > p2.x:
                 return (p1.y - p2.y) / (p1.x - p2.x)
@@ -77,6 +81,7 @@ if __name__ == '__main__':
         def yofseg(p1, p2, some_x):
             return slope(p1, p2) * (some_x - p1.x) + p1.y
 
+        # Calculating shifts for easier comparison
         dx = 0
         dy = 0
         if p1.x > p2.x:
@@ -86,6 +91,7 @@ if __name__ == '__main__':
             dx = p2.x - p1.x
             dy = p2.y - p1.y
 
+        #
         if ((dx > 0) and (dy < 0)) and p3.y < yofseg(p1, p2, p3.x):
             return Point(const, (-(const - center.x)) / (slope(t.p1, t.p2)) + center.y)
 
@@ -104,9 +110,9 @@ if __name__ == '__main__':
     Voronoi_vertices = []
     Voronoi_edges = []
 
-    for t in triangles:  # Generating edges of Voronoi + vertexes
+    for i, t in enumerate(triangles):  # Generating edges of Voronoi + vertexes
         if t.t1 == None:
-            Voronoi_vertices.append(cntoftrg(t))
+            Voronoi_vertices.append([cntoftrg(t), i])
             Voronoi_edges.append(cntoftrg(t))
             Voronoi_edges.append(infpoint(t.p1, t.p2, t.p3, cntoftrg(t)))
             Voronoi_edges.append(cntoftrg(t))
@@ -115,7 +121,7 @@ if __name__ == '__main__':
             Voronoi_edges.append(cntoftrg(t.t3))
             continue
         if t.t2 == None:
-            Voronoi_vertices.append(cntoftrg(t))
+            Voronoi_vertices.append([cntoftrg(t), i])
             Voronoi_edges.append(cntoftrg(t))
             Voronoi_edges.append(cntoftrg(t.t1))
             Voronoi_edges.append(cntoftrg(t))
@@ -124,7 +130,7 @@ if __name__ == '__main__':
             Voronoi_edges.append(cntoftrg(t.t3))
             continue
         if t.t3 == None:
-            Voronoi_vertices.append(cntoftrg(t))
+            Voronoi_vertices.append([cntoftrg(t), i])
             Voronoi_edges.append(cntoftrg(t))
             Voronoi_edges.append(cntoftrg(t.t1))
             Voronoi_edges.append(cntoftrg(t))
@@ -133,7 +139,7 @@ if __name__ == '__main__':
             Voronoi_edges.append(infpoint(t.p2, t.p3, t.p1, cntoftrg(t)))
             continue
         else:
-            Voronoi_vertices.append(cntoftrg(t))
+            Voronoi_vertices.append([cntoftrg(t), i])
             Voronoi_edges.append(cntoftrg(t))
             Voronoi_edges.append(cntoftrg(t.t1))
             Voronoi_edges.append(cntoftrg(t))
@@ -143,7 +149,7 @@ if __name__ == '__main__':
 
     # Plotting both points and triangles (with duplicates)
     for i in range(0, len(Voronoi_vertices)):
-        plt.plot(Voronoi_vertices[i].x, Voronoi_vertices[i].y, 'ro--')
+        plt.plot(Voronoi_vertices[i][0].x, Voronoi_vertices[i][0].y, 'ro--')
     for i in range(0, len(Voronoi_edges), 2):
         plt.plot([Voronoi_edges[i].x, Voronoi_edges[i + 1].x],
                  [Voronoi_edges[i].y, Voronoi_edges[i + 1].y], 'r')
@@ -151,4 +157,75 @@ if __name__ == '__main__':
     plt.xlim([axes[0], axes[1]])
     plt.ylim([axes[2], axes[3]])
     plt.show()
-    print('Done')
+    print('Done part 1, Starting Part 2 (interpolation)')
+
+    print('Please input the value of X:')
+    input_x = float(input())
+    print('Please input the value of Y:')
+    input_y = float(input())
+    newpoint = Point(input_x, input_y)
+    print('Point P(', newpoint.x, ',', newpoint.y, ') Created')
+    type_intr = 0
+    while True:
+        if (type_intr == 1) or (type_intr == 2) or (type_intr == 3) or (type_intr == 4):
+            break
+        print('Please choose type of interpolation: NB - 1, IDW - 2, Area - 3, Plane - 4')
+        type_intr = int(input())
+    print('Your choose:', type_intr)
+
+
+    def distance(p1, p2):
+        return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+
+
+    if type_intr == 1:  # Case 1 Nearest Neighbor
+        temp_dist = const
+        temp_trgl_id = None
+        for cell in Voronoi_vertices:  # Look to which cell new point is closer
+            if temp_dist > distance(newpoint, cell[0]):
+                temp_dist = distance(newpoint, cell[0])
+                temp_trgl_id = cell[1]
+        temp_dist = const
+        if temp_dist > distance(newpoint, triangles[temp_trgl_id].p1):  # Checking each point in triangle that we found
+            temp_dist = distance(newpoint, triangles[temp_trgl_id].p1)
+            newpoint.z = triangles[temp_trgl_id].p1.z
+        if temp_dist > distance(newpoint, triangles[temp_trgl_id].p2):  # Checking each point in triangle that we found
+            temp_dist = distance(newpoint, triangles[temp_trgl_id].p1)
+            newpoint.z = triangles[temp_trgl_id].p2.z
+        if temp_dist > distance(newpoint, triangles[temp_trgl_id].p3):  # Checking each point in triangle that we found
+            temp_dist = distance(newpoint, triangles[temp_trgl_id].p1)
+            newpoint.z = triangles[temp_trgl_id].p3.z
+
+    if type_intr == 2:  # Case 2 IDW
+        temp_dist = const
+        temp_trgl_id = None
+        for cell in Voronoi_vertices:  # Look to which cell new point is closer
+            if temp_dist > distance(newpoint, cell[0]):
+                temp_dist = distance(newpoint, cell[0])
+                temp_trgl_id = cell[1]
+        newpoint.z = (distance(newpoint, triangles[temp_trgl_id].p1) * triangles[temp_trgl_id].p1.z + distance(newpoint,
+                                                                                                               triangles[
+                                                                                                                   temp_trgl_id].p2) *
+                      triangles[temp_trgl_id].p2.z + distance(newpoint, triangles[temp_trgl_id].p3) * triangles[
+                          temp_trgl_id].p3.z) / (distance(newpoint, triangles[temp_trgl_id].p1) + distance(newpoint,
+                                                                                                           triangles[
+                                                                                                               temp_trgl_id].p2)
+                                                 + distance(newpoint, triangles[temp_trgl_id].p3))
+    if type_intr == 3:  # AutoCad solution!
+        print('Manual solution')
+    if type_intr == 4:  # Case 4 Plane
+        temp_dist = const
+        tti = None  # Temp triangle id
+        for cell in Voronoi_vertices:  # Look to which cell new point is closer
+            if temp_dist > distance(newpoint, cell[0]):
+                temp_dist = distance(newpoint, cell[0])
+                tti = cell[1]
+        p1p2 = np.array([triangles[tti].p2.x - triangles[tti].p1.x, triangles[tti].p2.y - triangles[tti].p1.y,
+                         triangles[tti].p2.z - triangles[tti].p1.z])
+        p1p3 = np.array([triangles[tti].p3.x - triangles[tti].p1.x, triangles[tti].p3.y - triangles[tti].p1.y,
+                         triangles[tti].p3.z - triangles[tti].p1.z])
+        abc = p1p2 * p1p3  # Creating a b c of plane
+        d = -(abc[0] * triangles[tti].p1.x + abc[1] * triangles[tti].p1.y + abc[2] * triangles[tti].p1.z)  # d of plane
+        newpoint.z = -(abc[0] * newpoint.x + abc[1] * newpoint.y + d) / (abc[2])  # Inserting new point into function of plane
+
+    print('The interpolation result is:', newpoint.z)
